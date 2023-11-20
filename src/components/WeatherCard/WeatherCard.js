ea13081c7fca2c2d8259f9e9e89675cbc705716a
okantas <okantas954@gmail.com>
2023-11-21 02:34:07 +0300

    card design is completed

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -4,17 +4,33 @@ import "./WeatherCard.css";
 export default class WeatherCard extends Component {
   render() {
     return (
-      <div className="card-container">
+      <container className="card-container">
         <div className="card">
           <div className="first-line">
-            <img className="sun-image" src={SunSVG} alt="sun image" />
+            <img className="sun-image" src={SunSVG} alt="sun" />
             <div className="location-and-date">
               <div className="location">Tepebaşı, Eskişehir, TR</div>
-              <div className="date">21/11/2023</div>
+              <div className="date">Salı 21/11/2023</div>
+            </div>
+          </div>
+
+          <div className="second-line">
+            <div className="degree">15</div>
+            <div className="type-of-degree">°C</div>
+          </div>
+
+          <div className="last-line">
+            <div className="left-part">
+              <div className="visibility">Visibility: 10km</div>
+              <div className="humidity">Humidity: 10km</div>
+            </div>
+            <div className="right-part">
+              <div className="feels-like">Feels Like: 10km</div>
+              <div className="wind">Wind: 10km</div>
             </div>
           </div>
         </div>
-      </div>
+      </container>
     );
   }
 }
